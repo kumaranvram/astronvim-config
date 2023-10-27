f2fbddee7a99434424e796a49710ff8d3ca28f39
KV <kumaran.venkataraman@shopify.com>
2023-10-26 23:33:17 -0400

    Add ruby toolkit

--- plugins/core.lua
+++ plugins/core.lua
@@ -74,4 +74,13 @@ return {
   --     }, { mode = "n", prefix = "<leader>" })
   --   end,
   -- },
+    {
+      'otavioschwanck/ruby-toolkit.nvim',
+      dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-treesitter/nvim-treesitter-textobjects" },
+      keys = {
+        { "<leader>mv", "<cmd>lua require('ruby-toolkit').extract_variable()<CR>", desc = "Extract Variable", mode = { "v" } },
+        { "<leader>mf", "<cmd>lua require('ruby-toolkit').extract_to_function()<CR>", desc = "Extract To Function", mode = { "v" } },
+        { "<leader>mw", "<cmd>lua require('ruby-toolkit').create_function_from_text()<CR>", desc = "Create Function from item on cursor" },
+      }
+    },
 }
